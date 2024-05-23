8ebb37ceb5ac6b62c812a72f5d2f9c793e7e5632
scaffnull <mathiasahman2@gmail.com>
2024-05-23 07:32:12 +0200

    added models to admin view

--- htmx_tests/admin.py
+++ htmx_tests/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
-from .models import City, Car, CarPool, CarType
+from .models import (
+    City,
+    Car,
+    CarPool, 
+    CarType, 
+    Employee, 
+    EmpPool, 
+    Resturant,)
 # Register your models here.
 
 admin.site.register(City)
 admin.site.register(Car)
 admin.site.register(CarPool)
 admin.site.register(CarType)
+admin.site.register(Employee)
+admin.site.register(EmpPool)
+admin.site.register(Resturant)
