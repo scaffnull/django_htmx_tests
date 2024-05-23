05d7832f4c0278df86fe6af076fe99c5010315d7
scaffnull <mathiasahman2@gmail.com>
2024-05-23 12:53:42 +0200

    modify/added url for emppool

--- htmx_tests/urls.py
+++ htmx_tests/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     path("emp_pool/", views.emp_pool, name="emp_pool"),
     path("emp_pool/<int:pk>/", views.emp_pool_workers, name="emp_pool_workers"),
     path("emp_pool/<int:pk>/<int:rest_id>/add/", views.emp_pool_add, name="emp_pool_add"),
-    path("emp_pool/<int:pk>/remove/", views.emp_pool_remove, name="emp_pool_remove"),
+    path("emp_pool/<int:pk>/<int:rest_id>/remove/", views.emp_pool_remove, name="emp_pool_remove"),
 ]
