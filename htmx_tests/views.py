24df13aa748f51377057863e72aef7eb3cea26e4
scaffnull <mathiasahman2@gmail.com>
2024-05-28 14:09:30 +0200

    added social media views section

--- htmx_tests/views.py
+++ htmx_tests/views.py
@@ -7,7 +7,10 @@ from .models import (
     CarPool,
     Employee,
     Resturant,
-    EmpPool)
+    EmpPool,
+    SocialMedia,
+    Category)
+from .filters import CategoryFilter
 
 # Create your views here.
 def index(request):
@@ -109,3 +112,72 @@ def emp_pool_remove(request, pk, rest_id):
             'resturantSelected',
             after="swap")
 
+""" END SECTION"""
+
+"""
+SECTION SOCIAL MEDIA, use with bool field, either follow or not follow from a list 
+with a click
+"""
+
+'''
+#TODO: Fix the issue when no category is selected and follow/unfollow htmx request is made
+Right now it returns the filtered result (which works if there is a category filtered)
+'''
+
+def social_media(request):
+    category_filter = CategoryFilter(request.GET,
+                            queryset=SocialMedia.objects.none().order_by('name'))
+    social = category_filter.qs
+    selected_value = category_filter.data.get('category')
+    print(selected_value)
+    if request.GET:
+        category_filter = CategoryFilter(request.GET,
+                                         queryset=SocialMedia.objects.order_by('name'))
+        social = category_filter.qs
+        following_count = social.filter(is_following=True).count()
+    #Making sure the following count only is showing True results
+    following_count = social.filter(is_following=True).count()
+    return render(request, 'partials/social_media.html', {
+        'form':category_filter.form,
+        'social': social,
+        'following_count':following_count,
+    })
+
+def social_media_list(request, selected_value):
+    category_filter = CategoryFilter(request.GET,
+                            queryset=SocialMedia.objects.none().order_by('name'))
+    selected_value = category_filter.data.get('category')   
+    social = SocialMedia.objects.filter(category=selected_value)
+
+    return render(request, 'partials/social_media_htmx.html', {
+        'social':social,
+    })   
+
+def social_media_unfollow(request, pk):
+    name = get_object_or_404(SocialMedia, pk=pk)
+    if request.htmx:
+        name.is_following = False
+        name.save()
+        social = SocialMedia.objects.filter(category=name.category) 
+        following_count = social.filter(is_following=True).count()
+        return render(request, 'partials/social_media_htmx.html', {
+            'social':social,
+            'following_count':following_count
+        })
+
+def social_media_follow(request, pk):
+    name = get_object_or_404(SocialMedia, pk=pk)
+    print(name)
+    if request.htmx:
+        name.is_following = True
+        name.save()
+        social = SocialMedia.objects.filter(category=name.category)
+        following_count = social.filter(is_following=True).count()
+        return render(request, 'partials/social_media_htmx.html', {
+            'social':social,
+            'following_count':following_count
+        })
+
+'''
+END SOCIAL MEDIA SECTION
+'''
